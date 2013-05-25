8696d21947cc2485d3bb5d4900b1c9e7b7d3a781
Kam Kasravi <kamkasravi@yahoo.com>
2013-05-24 22:20:12 -0700

    uprev high-level consumer

--- src/storm/kafka/ConsumerStream.java
+++ src/storm/kafka/ConsumerStream.java
@@ -1,6 +1,11 @@
 package storm.kafka;
 
+import java.util.List;
+
+import storm.kafka.PartitionManager.KafkaMessageId;
+
 import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.utils.Utils;
 import kafka.consumer.ConsumerIterator;
 import kafka.consumer.KafkaStream;
 import kafka.message.Message;
@@ -11,10 +16,13 @@ public class ConsumerStream implements Runnable {
     private KafkaStream<byte[],byte[]> m_stream;
     private int m_threadNumber;
     SpoutOutputCollector collector;
+    SpoutConfig _spoutConfig;
+    String host;
 
     public ConsumerStream(KafkaStream<byte[],byte[]> a_stream, int a_threadNumber) {
         m_threadNumber = a_threadNumber;
         m_stream = a_stream;
+        host = "localhost";
     }
 
     
@@ -26,6 +34,12 @@ public class ConsumerStream implements Runnable {
         	byte[] data = next.message();
         	Message message = new Message(data);
             System.out.println("Thread " + m_threadNumber + ": " + message.payload().toString());
+            Iterable<List<Object>> tups = _spoutConfig.scheme.deserialize(Utils.toByteArray(message.payload()));
+            if(tups!=null) {
+                for(List<Object> tup: tups) {
+                    collector.emit(tup, new KafkaMessageId(new GlobalPartitionId(new HostPort(host), next.partition()), next.offset()));
+                }
+            }
         }
         System.out.println("Shutting down Thread: " + m_threadNumber);
 	}
